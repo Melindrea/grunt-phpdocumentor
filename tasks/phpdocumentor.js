df9b28c8374923d4dc7a4e63ae06ea67047a6fe1
Marie Hogebrandt <iam@mariehogebrandt.se>
2013-12-08 00:38:34 +0100

    Adding support for ignore

--- tasks/phpdocumentor.js
+++ tasks/phpdocumentor.js
@@ -10,11 +10,11 @@
 
 module.exports = function(grunt) {
 
-    var ChildProcess = require('child_process'), 
-        Util = require('util'), 
+    var ChildProcess = require('child_process'),
+        Util = require('util'),
         Path = require('path'),
         _ = require('lodash')._;
-    
+
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
     grunt.registerMultiTask('phpdocumentor', 'Runs the PHPDocumentor documentation generator tool.', function() {
@@ -22,12 +22,12 @@ module.exports = function(grunt) {
         var options = {};
         // Merge task-specific and/or target-specific options with these defaults.
         _.extend(options, {
-                target : '',
+                target : 'docs',
                 filename : '',
-                directory : '',
+                directory : './',
                 encoding : '',
                 extensions : '',
-                ignore : '',
+                ignore : false,
                 hidden : '',
                 'ignore-symlinks' : '',
                 markers : '',
@@ -44,53 +44,55 @@ module.exports = function(grunt) {
             },
             this.data
         );
-        
+
         var done = this.async();
-        
+
         // Checks if the provided Phpdocumentor command name is valid
         if(options.command !== undefined &&
-           options.command !== 'help' && 
-           options.command !== 'list' && 
-           options.command !== 'parse' && 
-           options.command !== 'run' && 
+           options.command !== 'help' &&
+           options.command !== 'list' &&
+           options.command !== 'parse' &&
+           options.command !== 'run' &&
            options.command !== 'transform' &&
-           options.command !== 'project:parse' && 
-           options.command !== 'project:run' && 
+           options.command !== 'project:parse' &&
+           options.command !== 'project:run' &&
            options.command !== 'project:transform' &&
-           options.command !== 'template:generate' && 
-           options.command !== 'template:list' && 
+           options.command !== 'template:generate' &&
+           options.command !== 'template:list' &&
            options.command !== 'template:package') {
-            
+
             grunt.log.error(Util.format('Phpdocumentor does not provide any command named \'%s\' !', this.data.command));
             done(false);
-            
-        }
-        
-        // path to the phar file
-        var phpDocumentorCommand = options.bin        || Path.resolve(__dirname, '..', 'bin', 'phpdoc'),
-            target               = options.target     || 'docs',
-            directory            = options.directory  || './';
 
-        phpDocumentorCommand += ' --target=' + target;
-        phpDocumentorCommand += ' --directory=' + directory;
-        
-        grunt.log.write(phpDocumentorCommand);
-        
+        }
+
+        // path to the phar file
+        var phpDocumentorCommand = options.bin        || Path.resolve(__dirname, '..', 'bin', 'phpdoc');
+
+        phpDocumentorCommand += ' --target=' + options.target;
+        phpDocumentorCommand += ' --directory=' + options.directory;
+
+        if (options.ignore !== false) {
+            phpDocumentorCommand += ' --ignore=' + options.ignore.join(',');
+        }
+
+        grunt.log.writeln(phpDocumentorCommand);
+
         var childProcess = ChildProcess.exec(phpDocumentorCommand, function(error, stdout, stderr) {
-            
+
             grunt.log.writeln(error);
             grunt.log.writeln(stdout);
             grunt.log.writeln(stderr);
         });
-        
+
         childProcess.on('exit', function(code) {
-            
+
             if (code > 0) {
-                
+
                 grunt.log.error(Util.format('Exited with code: %d.', code));
-                
+
                 return done(false);
-            
+
             }
 
             grunt.verbose.ok(Util.format('Exited with code: %d.', code));
